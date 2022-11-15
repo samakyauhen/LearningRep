3fa3794fd08bc9f2d25c5b307f47efd3f97d8108
samakyauhen <samakyauhen89@gmail.com>
2022-11-15 18:06:13 -0500

    FAQ_Creation

---
+++ bingo/src/views/FAQ.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <FAQ msg="Belshina Bobruisk"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FAQ from '@/components/FAQ.vue';
+
+export default {
+  components: {
+    FAQ,
+  },
+};
+</script>
